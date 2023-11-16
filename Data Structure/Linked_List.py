ee9245c985726634f7c966dd4fd0146a18a4dfa4
unknown <prakashsatvi00@gmail.com>
2023-11-16 17:08:26 +0530

    create append, delete and print method

--- Data Structure/Linked_List.py
+++ Data Structure/Linked_List.py
@@ -3,5 +3,38 @@ class Node:
         self.data = data
         self.next = None
 
-node1 = Node(10)
-print(node1)
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+
+        last_node.next = new_node
+
+    def del_begin_node(self):
+        if self.head is not None:
+            self.head = self.head.next
+
+    def print_list(self):
+        current_node = self.head
+
+        while current_node is not None:
+            print(current_node.data, end=' -> ')
+            current_node = current_node.next
+
+
+linkedlist = LinkedList()
+linkedlist.append(1)
+linkedlist.append(2)
+linkedlist.append(3)
+linkedlist.del_begin_node()
+linkedlist.append(4)
+print(linkedlist.print_list())
