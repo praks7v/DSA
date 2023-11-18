5ce3d3f3c2ab916fd50c4d452d7a964dda85e8a5
Praks <prakashsatvi00@gmail.com>
2023-11-18 20:31:23 +0530

    created a Doubly Linked List and documented the both Singly and Doubly Linked List Code

--- Data Structure/Doubly_linkedlist.py
+++ Data Structure/Doubly_linkedlist.py
@@ -1,33 +1,189 @@
 class Node:
     def __init__(self, data):
+        """
+        Class representing a doubly linked list node.
+
+        Parameters:
+        - data: The data stored in the node.
+        """
         self.data = data
-        self.prev = None
-        self.next = None
+        self.prev = None  # Reference to the previous node
+        self.next = None  # Reference to the next node
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        """
+        Class representing a doubly linked list.
+
+        Attributes:
+        - head: The head (first node) of the doubly linked list.
+        """
+        self.head = None
+
+    def append_end(self, data):
+        """
+        Appends a new node with the given data at the end of the doubly linked list.
+
+        Parameters:
+        - data: The data to be stored in the new node.
+        """
+        new_node = Node(data)
+        if not self.head:
+            # If the list is empty, the new node becomes the head
+            self.head = new_node
+            return
+
+        current_node = self.head
+        while current_node.next is not None:
+            current_node = current_node.next
+
+        current_node.next = new_node
+        new_node.prev = current_node
+
+    def append_begin(self, data):
+        """
+        Appends a new node with the given data at the beginning of the doubly linked list.
+
+        Parameters:
+        - data: The data to be stored in the new node.
+        """
+        new_node = Node(data)
+        if not self.head:
+            # If the list is empty, the new node becomes the head
+            self.head = new_node
+            return
+
+        new_node.next = self.head
+        self.head.prev = new_node
+        self.head = new_node
+
+    def append_between(self, data, after_x):
+        """
+        Inserts a new node with the given data after the node with data equal to after_x.
+
+        Parameters:
+        - data: The data to be stored in the new node.
+        - after_x: The data value after which the new node should be inserted.
+        """
+        new_node = Node(data)
+
+        current_node = self.head
+        while current_node is not None:
+            if after_x == current_node.data:
+                break
+            current_node = current_node.next
+
+        if current_node is None:
+            print('Node not found')
+        else:
+            new_node.next = current_node.next
+            new_node.prev = current_node
+            current_node.next.prev = new_node
+            current_node.next = new_node
+
+    def del_begin(self):
+        """Deletes the first node in the doubly linked list."""
+        if not self.head:
+            return
+
+        if self.head.next:
+            self.head = self.head.next
+            self.head.prev = None
+        else:
+            self.head = None
+
+    def del_end(self):
+        """Deletes the last node in the doubly linked list."""
+        if not self.head:
+            return
+
+        if not self.head.next:
+            # If there is only one node in the list
+            self.head = None
+            return
+
+        current_node = self.head
+        while current_node.next.next:
+            current_node = current_node.next
+
+        current_node.next = None
+
+    def del_by_value(self, del_node):
+        """
+        Deletes the node with the given data value from the doubly linked list.
+
+        Parameters:
+        - del_node: The data value of the node to be deleted.
+        """
+        if not self.head:
+            return
+        if self.head.next is None:
+            if self.head.data == del_node:
+                self.head = None
+            else:
+                print('Given node is not present in Linked List')
+            return
+
+        if self.head.data == del_node:
+            self.head = self.head.next
+            self.head.prev = None
+
+        current_node = self.head
+        while current_node.next is not None:
+            if del_node == current_node.data:
+                break
+            current_node = current_node.next
+
+        if current_node.next:
+            current_node.next.prev = current_node.prev
+            current_node.prev.next = current_node.next
+        else:
+            if current_node.data == del_node:
+                current_node.prev.next = None
+            else:
+                print("Given node is not present in Linked List")
 
     def print_forward(self):
-        node = self
+        """Prints the doubly linked list in forward direction."""
+        if not self.head:
+            return print("Empty Linked List")
+
+        node = self.head
         while node is not None:
-            print(node.data, end=' - ')
+            print(node.data, end=' <-> ')
             node = node.next
         print('None')
 
     def print_backward(self):
-        node = self.next
+        """Prints the doubly linked list in backward direction."""
+        if not self.head:
+            return print("Empty Linked List")
+
+        node = self.head
         while node.next is not None:
             node = node.next
 
         while node is not None:
-            print(node.data, end=' - ')
+            print(node.data, end=' <-> ')
             node = node.prev
         print('None')
 
 
-node1 = Node(20)
-node2 = Node(10)
-node3 = Node(5)
-node1.next = node2
-node2.prev = node1
-node2.next = node3
-node3.prev = node2
-node1.print_forward()
-node1.print_backward()
+# Example Usage:
+if __name__ == '__main__':
+    doubly_list = DoublyLinkedList()
+    doubly_list.append_end(1)
+    doubly_list.append_end(2)
+    doubly_list.append_end(4)
+    doubly_list.append_begin(0)
+    doubly_list.append_between(3, 2)
+    doubly_list.del_begin()
+    doubly_list.del_end()
+    doubly_list.del_by_value(3)
+
+    print("\nForward traversal:")
+    doubly_list.print_forward()
+
+    print("\nBackward traversal:")
+    doubly_list.print_backward()
--- Data Structure/Singly_linkedlist.py
+++ Data Structure/Singly_linkedlist.py
@@ -1,14 +1,35 @@
 class Node:
+    """
+    Node class represents a node in a linked list.
+
+    Attributes:
+        data: The data stored in the node.
+        next: Reference to the next node in the linked list.
+    """
+
     def __init__(self, data):
         self.data = data
         self.next = None
 
 
 class LinkedList:
+    """
+    LinkedList class represents a singly linked list.
+
+    Attributes:
+        head: Reference to the first node in the linked list.
+    """
+
     def __init__(self):
         self.head = None
 
-    def append(self, data):
+    def append_end(self, data):
+        """
+        Appends a new node with the given data to the end of the linked list.
+
+        Parameters:
+            data: The data to be added to the new node.
+        """
         new_node = Node(data)
         if not self.head:
             self.head = new_node
@@ -16,15 +37,27 @@ class LinkedList:
         last_node = self.head
         while last_node.next:
             last_node = last_node.next
-
         last_node.next = new_node
 
-    def add_begin_node(self, data):
+    def append_begin(self, data):
+        """
+        Adds a new node with the given data to the beginning of the linked list.
+
+        Parameters:
+            data: The data to be added to the new node.
+        """
         new_node = Node(data)
         new_node.next = self.head
         self.head = new_node
 
-    def add_after_node(self, data, x):
+    def append_after(self, data, x):
+        """
+        Adds a new node with the given data after a node with the specified value 'x'.
+
+        Parameters:
+            data: The data to be added to the new node.
+            x: The value of the node after which the new node should be added.
+        """
         node = self.head
         while node is not None:
             if x == node.data:
@@ -37,7 +70,14 @@ class LinkedList:
             new_node.next = node.next
             node.next = new_node
 
-    def add_before_node(self, data, x):
+    def append_before(self, data, x):
+        """
+        Adds a new node with the given data before a node with the specified value 'x'.
+
+        Parameters:
+            data: The data to be added to the new node.
+            x: The value of the node before which the new node should be added.
+        """
         if self.head is None:
             return
 
@@ -62,10 +102,12 @@ class LinkedList:
             node.next = new_node
 
     def del_begin_node(self):
+        """Deletes the first node in the linked list."""
         if self.head is not None:
             self.head = self.head.next
 
     def del_end_node(self):
+        """Deletes the last node in the linked list."""
         if self.head is None:
             return
         node = self.head
@@ -74,6 +116,12 @@ class LinkedList:
         node.next = None
 
     def del_node(self, x):
+        """
+        Deletes the node with the specified value 'x' from the linked list.
+
+        Parameters:
+            x: The value of the node to be deleted.
+        """
         if self.head is None:
             return
         if self.head.data == x:
@@ -87,22 +135,26 @@ class LinkedList:
         node.next = node.next.next
 
     def print_list(self):
+        """Prints the elements of the linked list."""
         current_node = self.head
         while current_node is not None:
             print(current_node.data, end=' -> ')
             current_node = current_node.next
+        print('None')
 
 
-linkedlist = LinkedList()
-linkedlist.append(1)
-linkedlist.append(2)
-linkedlist.append(3)
-linkedlist.del_begin_node()
-linkedlist.append(6)
-linkedlist.add_begin_node(1)
-linkedlist.add_begin_node(8)
-linkedlist.add_after_node(4, 3)
-linkedlist.add_before_node(5, 6)
-linkedlist.del_end_node()
-linkedlist.del_node(8)
-print(linkedlist.print_list())
+# Example Usage:
+if __name__ == '__main__':
+    linkedlist = LinkedList()
+    linkedlist.append_end(1)
+    linkedlist.append_end(2)
+    linkedlist.append_end(3)
+    linkedlist.del_begin_node()
+    linkedlist.append_end(6)
+    linkedlist.append_begin(1)
+    linkedlist.append_begin(8)
+    linkedlist.append_after(4, 3)
+    linkedlist.append_before(5, 6)
+    linkedlist.del_end_node()
+    linkedlist.del_node(8)
+    linkedlist.print_list()
